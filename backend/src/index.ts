c917c9b9e4520c58daac8d7dbb2018ac0626dcc8
kenzoutenma <117408562+kenzoutenma@users.noreply.github.com>
2026-06-18 16:09:31 +0300

    fix (back): cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,20 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 
 import quizRoute from "./controller/quiz.js";
 
 const app: Application = express();
 const PORT = process.env.PORT ?? "3001";
+
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173", "http://localhost:8080"];
+
+app.use(
+  cors({
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: allowedOrigins,
+  }),
+);
 
 app.use(express.json());
 
